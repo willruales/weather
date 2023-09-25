4e2f763d717fd1978ae244fa84b68b7465f78e78
William Ruales <ruales@hotmail.co.uk>
2023-09-25 17:50:14 +0100

    promise version

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-async function fetchWeatherData() {
-    try {
-        const city = document.getElementById("cityInput").value;
-        if (!city) {
-            throw new Error("Please enter a city name.");
-        }
+function fetchWeatherData() {
+    const city = document.getElementById("cityInput").value;
 
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1ed9302300424f6bbb7122401232109&q=${city}`, { mode: 'cors' });
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-
-        const data = await response.json();
-
-        const weatherDataDiv = document.querySelector(".result");
-        weatherDataDiv.innerHTML = `
-        <h2>Weather in ${data.location.name}, ${data.location.country}</h2>
-        <p>Temperature: ${data.current.temp_c}°C</p>
-        <p class="trial">Condition: ${data.current.condition.text}</p>
-      `;
-    } catch (error) {
-        console.error(error.message);
+    if (!city) {
+        return Promise.reject(new Error("Please enter a city name."));
     }
+
+    return fetch(`https://api.weatherapi.com/v1/current.json?key=1ed9302300424f6bbb7122401232109&q=${city}`, { mode: 'cors' })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const weatherDataDiv = document.querySelector(".result");
+            weatherDataDiv.innerHTML = `
+          <h2>Weather in ${data.location.name}, ${data.location.country}</h2>
+          <p>Temperature: ${data.current.temp_c}°C</p>
+          <p class="trial">Condition: ${data.current.condition.text}</p>
+        `;
+            return data;
+        })
+        .catch((error) => {
+            console.error(error.message);
+            throw error;
+        });
 }
+
+// Example usage:
+fetchWeatherData()
+    .then((data) => {
+        console.log("Weather data:", data);
+    })
+    .catch((error) => {
+        console.error("Error:", error.message);
+    });
